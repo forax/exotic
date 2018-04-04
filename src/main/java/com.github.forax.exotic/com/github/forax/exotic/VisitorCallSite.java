f2a52dcd608b53ac5b9095549b87411f49bca04b
forax <forax@univ-mlv.fr>
2018-04-04 21:01:25 +0200

    visitor: detect if inlining cache is too deep

--- src/main/java/com.github.forax.exotic/com/github/forax/exotic/VisitorCallSite.java
+++ src/main/java/com.github.forax.exotic/com/github/forax/exotic/VisitorCallSite.java
@@ -16,7 +16,7 @@ import java.util.Objects;
 import com.github.forax.exotic.Visitor.Visitlet;
 
 class VisitorCallSite extends MutableCallSite {
-  private static final MethodHandle FALLBACK, TYPECHECK;
+  private static final MethodHandle FALLBACK, TYPECHECK, FIND;
   static final MethodHandle VISIT;
   static {
     Lookup lookup = MethodHandles.lookup();
@@ -24,6 +24,7 @@ class VisitorCallSite extends MutableCallSite {
       FALLBACK = lookup.findVirtual(VisitorCallSite.class, "fallback", methodType(MethodHandle.class, Object.class));
       VISIT = lookup.findVirtual(Visitlet.class, "visit", methodType(Object.class, Visitor.class, Object.class, Object.class));
       TYPECHECK = lookup.findStatic(VisitorCallSite.class, "typecheck", methodType(boolean.class, Class.class, Object.class));
+      FIND = lookup.findStatic(VisitorCallSite.class, "find", methodType(MethodHandle.class, HashMap.class, Object.class));
     } catch (NoSuchMethodException | IllegalAccessException e) {
       throw new AssertionError(e);
     }
@@ -43,10 +44,22 @@ class VisitorCallSite extends MutableCallSite {
     };
   }
 
+  private final int depth;
+  private final VisitorCallSite callsite;
   private final HashMap<Class<?>, MethodHandle> map;
 
-  private VisitorCallSite(MethodType methodType, HashMap<Class<?>, MethodHandle> map) {
+  private VisitorCallSite(MethodType methodType, HashMap<Class<?>,MethodHandle> map) {
     super(methodType);
+    this.depth = 0;
+    this.callsite = this;
+    this.map = map;
+    setTarget(foldArguments(exactInvoker(methodType), FALLBACK.bindTo(this)));
+  }
+  
+  private VisitorCallSite(MethodType methodType, VisitorCallSite callsite, int depth, HashMap<Class<?>,MethodHandle> map) {
+    super(methodType);
+    this.depth = depth;
+    this.callsite = callsite;
     this.map = map;
     setTarget(foldArguments(exactInvoker(methodType), FALLBACK.bindTo(this)));
   }
@@ -58,15 +71,31 @@ class VisitorCallSite extends MutableCallSite {
     if (target == null) {
       throw new IllegalStateException("no visitlet register for type " + receiverClass.getName());
     }
-    MethodHandle guard = guardWithTest(TYPECHECK.bindTo(receiverClass),
-        target,
-        new VisitorCallSite(type(), map).dynamicInvoker());
-    setTarget(guard);
+    
+    if (depth == /*MAX_DEPTH*/ 10) {
+      callsite.setTarget(foldArguments(exactInvoker(type()), FIND.bindTo(map)));
+    } else {
+      MethodHandle guard = guardWithTest(TYPECHECK.bindTo(receiverClass),
+          target,
+          new VisitorCallSite(type(), callsite, depth + 1, map).dynamicInvoker());
+      setTarget(guard);
+    }
+    
     return target;
   }
 
   @SuppressWarnings("unused")
   private static boolean typecheck(Class<?> receiverClass, Object receiver) {
     return receiverClass == receiver.getClass();
+  }
+  
+  @SuppressWarnings("unused")
+  private static MethodHandle find(HashMap<Class<?>, MethodHandle> map, Object o) {
+    Class<?> receiverClass = o.getClass();
+    MethodHandle target = map.get(receiverClass);
+    if (target == null) {
+      throw new IllegalStateException("no visitlet register for type " + receiverClass.getName());
+    }
+    return target;
   }
 }
