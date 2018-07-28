256d3088f176242e684d5e018a6e8ccdf8e232ce
forax <forax@univ-mlv.fr>
2018-07-28 14:10:46 +0200

    an empty special build should act  as if there were no special build

--- pro_wrapper.java
+++ pro_wrapper.java
@@ -65,7 +65,7 @@ class pro_wrapper {
   
   private static Optional<String> specialBuild() {
     var specialBuild = System.getenv("PRO_SPECIAL_BUILD");
-    return Optional.ofNullable(specialBuild);
+    return Optional.ofNullable(specialBuild).filter(build -> !build.isEmpty());
   }
   
   private static String userHome() {
