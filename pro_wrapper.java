ae0c80624f621a1ef7ea547c0a5532eb7bed042f
forax <forax@univ-mlv.fr>
2019-05-11 23:27:05 +0200

    update to latest pro_wrapper

--- pro_wrapper.java
+++ pro_wrapper.java
@@ -3,33 +3,42 @@ import static java.lang.System.getProperty;
 import static java.nio.file.Files.createDirectories;
 import static java.nio.file.Files.delete;
 import static java.nio.file.Files.exists;
+import static java.nio.file.Files.getLastModifiedTime;
 import static java.nio.file.Files.getPosixFilePermissions;
 import static java.nio.file.Files.list;
+import static java.nio.file.Files.newBufferedReader;
 import static java.nio.file.Files.newOutputStream;
 import static java.nio.file.Files.readAllLines;
 import static java.nio.file.Files.setPosixFilePermissions;
+import static java.nio.file.Files.walk;
 import static java.nio.file.Files.walkFileTree;
 import static java.nio.file.Files.write;
 import static java.nio.file.attribute.PosixFilePermission.GROUP_EXECUTE;
 import static java.nio.file.attribute.PosixFilePermission.OTHERS_EXECUTE;
 import static java.nio.file.attribute.PosixFilePermission.OWNER_EXECUTE;
+import static java.util.Collections.reverseOrder;
+import static java.util.Comparator.comparing;
+import static java.util.function.Predicate.not;
 
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.io.UncheckedIOException;
+import java.net.InetAddress;
 import java.net.URL;
+import java.net.UnknownHostException;
 import java.nio.charset.StandardCharsets;
 import java.nio.file.FileVisitResult;
+import java.nio.file.Files;
 import java.nio.file.Path;
-import java.nio.file.Paths;
 import java.nio.file.SimpleFileVisitor;
 import java.nio.file.attribute.BasicFileAttributes;
-import java.nio.file.attribute.PosixFilePermission;
+import java.nio.file.attribute.FileTime;
 import java.util.Arrays;
 import java.util.Collections;
 import java.util.List;
 import java.util.Optional;
-import java.util.Set;
+import java.util.Properties;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 import java.util.stream.Stream;
@@ -56,30 +65,60 @@ class pro_wrapper {
     if (osName.indexOf("win") != -1) {
       return "cmd.exe";
     }
-    var shell = System.getenv("SHELL");
-    if (shell != null && !shell.isEmpty()) {
-      return shell;
-    }
-    return "/bin/sh";
+    return Optional.ofNullable(System.getenv("SHELL")).filter(not(String::isEmpty)).orElse("/bin/sh");
   }
   
-  private static Optional<String> specialBuild() {
+  private static Optional<String> specialBuild() throws IOException {
     var specialBuild = System.getenv("PRO_SPECIAL_BUILD");
-    return Optional.ofNullable(specialBuild).filter(build -> !build.isEmpty());
+    var path = Path.of("pro_wrapper_settings.txt");
+    var proSettings = new Properties();
+    if (exists(path)) {
+      try(var reader = newBufferedReader(path)) {
+        proSettings.load(reader);
+      }
+    }
+    return Optional.ofNullable(specialBuild).filter(not(String::isEmpty)).or(
+        () -> Optional.ofNullable(proSettings.getProperty("PRO_SPECIAL_BUILD")));
   }
   
   private static String userHome() {
     return System.getProperty("user.home");
   }
   
-  private static Optional<String> lastestReleaseVersion() throws IOException {
+  private static Optional<String> lastestReleaseVersionFromGitHub() throws IOException {
+    System.out.println("try to find latest release on Github ...");
     var url = new URL(GITHUB_API_RELEASES);
+    try {
+      InetAddress.getByName(url.getHost());  // is internet accessible ?
+    } catch(@SuppressWarnings("unused") UnknownHostException e) {
+      return Optional.empty();
+    }
     try(var input = url.openStream();
         var reader = new InputStreamReader(input, StandardCharsets.UTF_8);
         var buffered = new BufferedReader(reader, 8192)) {
       return buffered.lines()
         .flatMap(line -> Optional.of(PATTERN.matcher(line)).filter(Matcher::find).map(matcher -> matcher.group(1)).stream())
         .findFirst();
+    }
+  }
+  
+  private static FileTime lastModified(Path path) {
+    try {
+      return getLastModifiedTime(path);
+    } catch (IOException e) {
+      throw new UncheckedIOException(e);
+    }
+  }
+  
+  private static Optional<String> latestReleaseVersionFromCache(Path cache, String filename) {
+    System.out.println("try to find latest release in the cache ...");
+    try {
+      return walk(cache)
+          .filter(p -> p.getFileName().toString().equals(filename))
+          .sorted(reverseOrder(comparing(p -> lastModified(p))))
+          .findFirst().map(p -> p.getParent().getFileName().toString());
+    } catch (@SuppressWarnings("unused") IOException | UncheckedIOException e) {
+      return Optional.empty();
     }
   }
   
@@ -106,6 +145,12 @@ class pro_wrapper {
     System.out.println("");
   }
   
+  private static void setExecutionPermissions(Path path) throws IOException {
+    var permissions = getPosixFilePermissions(path);
+    Collections.addAll(permissions, OWNER_EXECUTE, GROUP_EXECUTE, OTHERS_EXECUTE);
+    setPosixFilePermissions(path, permissions);
+  }
+  
   private static void unpack(Path localPath, Path folder) throws IOException {
     System.out.println("unpack pro to " + folder);
     createDirectories(folder);
@@ -123,11 +168,15 @@ class pro_wrapper {
       }
     }
     
-    // make the command in pro/bin executable
+    // make the commands in pro/bin, and pro/lib executable
     for(var cmd: (Iterable<Path>)list(folder.resolve("pro").resolve("bin"))::iterator) {
-      Set<PosixFilePermission> permissions = getPosixFilePermissions(cmd);
-      Collections.addAll(permissions, OWNER_EXECUTE, GROUP_EXECUTE, OTHERS_EXECUTE);
-      setPosixFilePermissions(cmd, permissions);
+      setExecutionPermissions(cmd);
+    }
+    for(var path: (Iterable<Path>)list(folder.resolve("pro").resolve("lib"))::iterator) {
+      var fileName = path.getFileName().toString();
+      if (fileName.equals("jexec") || fileName.equals("jspawnhelper")) {
+        setExecutionPermissions(path);
+      }
     }
   }
   
@@ -164,22 +213,58 @@ class pro_wrapper {
     }
   }
   
+  public interface PathConsumer {
+    void accept(Path path) throws IOException;
+  }
+  
+  private static void retry(Path resource, int times, PathConsumer consumer) throws IOException {
+    if (times <= 0) {
+      throw new IllegalArgumentException("times <= 0");
+    }
+    IOException exception = null;
+    var count = times;
+    do {
+      try {
+        consumer.accept(resource);
+        return;
+      } catch(IOException e) {
+        if (exception == null) {
+          exception = new IOException();
+        }
+        exception.addSuppressed(e);
+        
+        // cleanup
+        Files.deleteIfExists(resource);
+        
+        System.out.println("download fails ... retry !");
+      }
+    } while(--count != 0);
+    throw exception;
+  }
+  
   private static int installAndRun(String[] args) throws IOException {
-    var release = lastestReleaseVersion().orElseThrow(() -> new IOException("latest release not found on Github"));
     var specialBuild = specialBuild().map(build -> '-' + build).orElse("");
     var filename = "pro-" + platform() + specialBuild + ".zip";
     
-    var cachePath = Paths.get(userHome(), ".pro", "cache", release, filename);
+    var cache = Path.of(userHome(), ".pro", "cache");
+    var release = lastestReleaseVersionFromGitHub()
+        .or(() -> latestReleaseVersionFromCache(cache, filename))
+        .orElseThrow(() -> new IOException("no release found !"));
+    
+    
+    System.out.println("require " + filename + " release " + release + " ...");
+    
+    var cachePath = cache.resolve(Path.of(release, filename));
     if (!exists(cachePath)) {
-      download(release, filename, cachePath);
+      retry(cachePath, 3, _cachedPath -> download(release, filename, _cachedPath));
     }
     
-    var releaseTxt = Paths.get("pro", "pro-release.txt");
-    if (!exists(releaseTxt) || !firstLine(releaseTxt).equals(release)) {
+    var releaseTxt = Path.of("pro", "pro-release.txt");
+    if (!exists(releaseTxt) || !firstLine(releaseTxt).equals(filename)) {
       deleteAllFiles(releaseTxt.getParent());
       
-      unpack(cachePath, Paths.get("."));
-      write(releaseTxt, List.of(release));
+      unpack(cachePath, Path.of("."));
+      write(releaseTxt, List.of(filename));
     }
     
     return exec("pro/bin/pro", args);
@@ -190,7 +275,7 @@ class pro_wrapper {
       exit(installAndRun(args));
     } catch(IOException e) {
       System.err.println("i/o error " + e.getMessage() +
-          Optional.ofNullable(e.getStackTrace()).filter(stack -> stack.length > 0).map(stack -> " from " + stack[0]).orElse(""));
+          Optional.ofNullable(e.getStackTrace()).filter(stack -> stack.length > 0).map(stack -> " at " + stack[0]).orElse(""));
       exit(1);
     }
   }
